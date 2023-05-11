0d3786e14dc65c9fdc3220bd1b122d8843ddc469
lndsilva <lndsilva@hotmail.com>
2023-05-11 19:42:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/CadUsuActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadUsuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cad_usu_layout);
+    }
+}

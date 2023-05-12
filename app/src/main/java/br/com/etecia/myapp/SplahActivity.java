89f450050d6d422598a09c08e8c8c50ead95c14c
lndsilva <lndsilva@hotmail.com>
2023-05-11 21:12:35 -0300

    splash executando

--- app/src/main/java/br/com/etecia/myapp/SplahActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplahActivity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplahActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splah_layout);
+
+        //criando o carregamento da janela de login
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //abrindo a janela de login
+                startActivity(new Intent(getApplicationContext(),
+                        LoginActivity.class));
+                finish();//fechando a janela atual
+            }
+        }, 3000);
+
+
     }
 }

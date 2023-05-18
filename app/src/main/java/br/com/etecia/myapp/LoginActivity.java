c849196b02f657e4418ae97a037105353e9ba189
lndsilva <lndsilva@hotmail.com>
2023-05-18 19:08:38 -0300

    janela de login

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -3,12 +3,24 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnEntrar, btnSair;
+    TextInputEditText txtUsuario, txtSenha;
+    TextView lblCadastraUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtSenha = findViewById(R.id.txtSenha);
+        lblCadastraUsuario = findViewById(R.id.lblCadastrarUsuario);
     }
 }

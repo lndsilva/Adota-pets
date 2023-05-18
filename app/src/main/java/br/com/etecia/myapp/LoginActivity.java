fb85c74e9ff7abbe1646b3f771a2ee110812bbbd
lndsilva <lndsilva@hotmail.com>
2023-05-18 19:15:37 -0300

    janela de login entrada

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,16 +2,22 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import br.com.etecia.myapp.MenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnEntrar, btnSair;
     TextInputEditText txtUsuario, txtSenha;
     TextView lblCadastraUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +28,29 @@ public class LoginActivity extends AppCompatActivity {
         txtUsuario = findViewById(R.id.txtUsuario);
         txtSenha = findViewById(R.id.txtSenha);
         lblCadastraUsuario = findViewById(R.id.lblCadastrarUsuario);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

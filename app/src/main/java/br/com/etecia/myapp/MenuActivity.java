83144110cfc94c60b27a15eb9f1b0230968660d3
lndsilva <lndsilva@hotmail.com>
2023-05-18 21:23:34 -0300

    implementação onclicklistener menu

--- app/src/main/java/br/com/etecia/myapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuActivity.java
@@ -3,12 +3,31 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuActivity extends AppCompatActivity {
+    MaterialCardView idMenuPets, idMenuAlimentos,
+            idMenuDoacoes, idMenuServicos,
+            idMenuLocalizar, idMenuParceiros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        idMenuPets = findViewById(R.id.idMenuPets);
+        idMenuAlimentos = findViewById(R.id.idMenuAlimentos);
+        idMenuDoacoes = findViewById(R.id.idMenuDoacoes);
+        idMenuServicos = findViewById(R.id.idMenuServicos);
+        idMenuParceiros = findViewById(R.id.idMenuParceiros);
+        idMenuLocalizar = findViewById(R.id.idMenuLocalizar);
+
+
+
+
     }
+
+
 }

2f86c262d468d3a4a5909b623780c4f708cdbcaa
lndsilva <lndsilva@hotmail.com>
2023-05-18 21:07:31 -0300

    janelas para o menu

---
+++ app/src/main/java/br/com/etecia/myapp/ParceirosActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParceirosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parceiros_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myapp/PetsActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pets_layout);
+    }
+}

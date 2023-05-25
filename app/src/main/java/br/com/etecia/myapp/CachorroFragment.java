59f1685ce73a58312b90271a64678417b07b4187
lndsilva <lndsilva@hotmail.com>
2023-05-25 20:18:49 -0300

    fragment layout

---
+++ app/src/main/java/br/com/etecia/myapp/CachorroFragment.java
@@ -0,0 +1,26 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CachorroFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_cachorro, container, false);
+
+        //todos os comandos devem ficar aqui.
+
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myapp/GatoFragment.java
@@ -0,0 +1,23 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GatoFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gato, container, false);
+
+
+        return view;
+    }
+}
